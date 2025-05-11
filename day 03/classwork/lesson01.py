b60065a623e9ae30ba62f1353e1dc5bad9440e49
GioJoorj <giorgikaadze7@gmail.com>
2025-05-11 07:32:54 -0700

    fifty commit

--- day 03/classwork/lesson01.py
+++ day 03/classwork/lesson01.py
@@ -1,1 +1,10 @@
-#no classwork
+#camelCase არის დიდ ასოთი str
+#snake_case კი არის space გამოტოვება მაგრამ _ ვიყენებთ
+gameSnake = "hello world"
+Bmw = "Bayerische Motoren Werke"
+hello_world = "hello wrold 2"
+audi_a8 = "listen!"
+print(gameSnake)
+print(Bmw)
+print(hello_world)
+print(audi_a8)
